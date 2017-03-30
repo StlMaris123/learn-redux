023c940e16739e2ff7678aa7dabd4b1e96825caa
StlMaris123 <stellamarisnjage@gmail.com>
2017-03-30 16:05:22 +0300

    add children to component

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -9,5 +9,20 @@ import css from './styles/style.styl';
 // import components
 
 import Main from './components/Main'
+import Single from './components/Single'
+import PhotGrid from './components/PhotGrid'
 
-render(<Main/>, document.getElementById('root') );
+// import react router deps
+
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+    <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+    <IndexRoute component={PhotoGrid}></IndexRoute>
+    <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+    </Router>
+)
+
+render(<Main><p>Yo</p></Main>, document.getElementById('root') );
