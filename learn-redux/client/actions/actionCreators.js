707fc8ad1fbcf7b370ad360509683240bcdcd957
StlMaris123 <stellamarisnjage@gmail.com>
2017-04-18 21:42:27 +0300

    action creators for removing and adding comments

--- learn-redux/client/actions/actionCreators.js
+++ learn-redux/client/actions/actionCreators.js
@@ -1,10 +1,27 @@
 // increment
 
-function increment(index){
+export function increment(index){
   return {
     type: 'INCREMENT_LIKES',
     index
   }
 }
 // add comment
+
+export function addComment(postId, author, comment){
+  return {
+    type: 'ADD_COMMENT',
+    postId,
+    author,
+    comment
+  }
+}
 // remove comment
+
+export function removeComment(postId, i){
+  return {
+    type: 'REMOVE_COMMENT',
+    i,
+    postId
+  }
+}
